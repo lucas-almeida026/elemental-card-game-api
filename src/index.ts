4830245da62e26fc43417125d068fc768d66129f
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2021-11-13 08:52:51 -0300

    init + socket + express

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import http from 'http'
+
+const port = process.env.PORT || 8090
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.send('asd')
+})
+
+io.on('connection', (socket) => {
+  console.log(`new user connected with id ${socket.id}`)
+})
+
+server.listen(port, () => console.log(`socket listening as port ${port}`))
